718f367a453f404e058524ed3f05dea1116185c8
daviaquino87 <davi26031@gmail.com>
2023-02-14 13:02:55 -0300

    feat: create middleware by validate errors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+
+import { indexRouter } from "./routes/index.routes";
 
 import "./database";
 
 const app = express();
-app.listen(process.env.PORT || 300, () => {
+app.use(express.json());
+app.use(indexRouter);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error) {
+      return response.json({ error: "internal error server." });
+    }
+  }
+);
+
+app.listen(process.env.PORT || 3000, () => {
   console.log("Application running.");
 });
